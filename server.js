b589e1db5344383aecafa8cf3e9ce77b6bc406f0
CartVman <kurtvalderama11@gmail.com>
2022-08-15 14:28:39 +0200

    commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const HTTP_PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+
+app.use(express.static('code'));
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send('hello');
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
