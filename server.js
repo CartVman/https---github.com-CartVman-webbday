f13d667056058c10bc9eba1d232dfefb1c9b03bc
CartVman <kurtvalderama11@gmail.com>
2022-08-15 15:24:24 +0200

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const HTTP_PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
 
-app.use(express.static('code'));
+app.use(express.static(__dirname+"/bday message"));
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
