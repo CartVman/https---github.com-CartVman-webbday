b56b4f1673d651c5cee7c68acabaaa6d30b015aa
CartVman <kurtvalderama11@gmail.com>
2022-08-15 15:19:51 +0200

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static('code'));
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/bday message");
+    res.sendFile(__dirname+"/index.html");
 });
 
 // setup http server to listen on HTTP_PORT
